085f6ee7f8ec4603de4cc19af455815bc61a6b6e
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2022-05-08 18:10:32 +0300

    create contact Rest API

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\VisitorController;
+use App\Http\Controllers\Admin\ContactController;
+
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
 
+// Get Visitor
 Route::get('/get-visitor', [VisitorController::class, 'GetVisitorDetails']);
+
+// contact page route route
+Route::post('/post-contact', [ContactController::class, 'PostContactDetails']);
