426772065ab7feea6ad30c8c7a60d84e129107e3
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2022-05-09 23:03:36 +0300

    30 - Create Product List API Part 2

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Admin\VisitorController;
 use App\Http\Controllers\Admin\ContactController;
 use App\Http\Controllers\Admin\SiteInfoController;
 use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ProductListController;
 
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
@@ -23,3 +24,10 @@ Route::get('/all-site-info', [SiteInfoController::class, 'AllSiteInfo']);
 
 // all categories route
 Route::get('/all-category', [CategoryController::class, 'AllCategory']);
+
+// productList route
+Route::get('/productlistbyremark/{remark}', [ProductListController::class, 'ProductListByRemark']);
+
+Route::get('/productlistbycategory/{category}', [ProductListController::class, 'ProductListByCategory']);
+
+Route::get('/productlistbysubcategory/{category}/{subcategory}', [ProductListController::class, 'ProductListBySubcategory']);
