40530eebc0d6d7e1fc345f04ca2d18d5f6d7875c
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2022-06-05 13:39:15 +0300

    setup admin theme

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified'
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
     Route::get('/dashboard', function () {
-        return view('dashboard');
+        return view('admin.index');
     })->name('dashboard');
 });
