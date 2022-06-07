ae8b3c8cce8ac26395b60e9dc231552884108dde
Prisecaru Emilia <prisecaru.emilia@gmail.com>
2022-06-07 22:57:06 +0300

    Add Store Product Part 2

--- routes/web.php
+++ routes/web.php
@@ -106,7 +106,7 @@ Route::prefix('product')->group(function () {
 
     Route::get('/add', [ProductListController::class, 'AddProduct'])->name('add.product');
 
-    Route::post('/store', [SliderController::class, 'StoreSlider'])->name('slider.store');
+    Route::post('/store', [ProductListController::class, 'StoreProduct'])->name('product.store');
 
     Route::get('/edit/{id}', [SliderController::class, 'EditSlider'])->name('slider.edit');
 
